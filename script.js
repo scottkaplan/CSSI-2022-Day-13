84e01d6b1fce8993341d1c2a0116cb4fbb3823f0
Scott Kaplan <scott@kaplans.com>
2022-07-27 16:21:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const toppings = document.querySelectorAll(".topping");
+
+toppings.forEach((topping) => {
+  topping.addEventListener("click", (e) => {
+    console.log(topping.dataset);
+  });
+});
+
