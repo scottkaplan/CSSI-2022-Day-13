d78680cd9b7dcb1dd422fd90f13f6fde4123b3d2
Scott Kaplan <scott@kaplans.com>
2022-07-27 16:46:26 +0000

    Added feature to add the cost of the item in the order when the user pushes the checkout button.

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ toppings.forEach((topping) => {
   });
 });
 
+const checkoutButton = document.querySelector(".button");
+const answerHolder = document.querySelector("#total");
+let total = 0;
+ checkoutButton.addEventListener("click", (e) => {
+   ingredients.forEach((ingredient) => {
+    total += Number(ingredient.dataset.price);
+   });
+  answerHolder.innerHTML = `The total cost of your burger is $${total}`;
+ });
+
+ const ingredients = document.querySelectorAll(".order .topping");
+
